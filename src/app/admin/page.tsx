4c89e07c3c170cfb6ca7516bd564c3198e1c8c56
Baranbulduk <baranbulduk99@gmail.com>
2025-06-24 13:31:43 +0200

    Initial commit

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import Image from "next/image";
-import { FaLinkedin, FaEnvelope, FaPhone } from "react-icons/fa";
 
 interface Submission {
   name: string;
